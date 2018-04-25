5c8cd26cefd021f62e595b9b876eef37e6f91feb
Eric Dailey <eric.dailey@utexas.edu>
2018-04-25 06:37:20 -0500

    demo images

--- clone.py
+++ clone.py
@@ -2,6 +2,7 @@ from keras.models import model_from_json
 import csv
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 #read and store lines from the log file
 lines = []
@@ -56,8 +57,26 @@ model.add(Dense(10))
 model.add(Dense(1, name='OUTPUT'))
 
 model.compile(loss='mean_squared_logarithmic_error', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=100)
-print(model.predict(images))
+history = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=100)
+print(history.history.keys())
+# summarize history for accuracy
+'''plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+plt.savefig('model_accuracy')'''
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+plt.savefig('model_loss')
 model.save('model.h5')
 
 def freeze_session(session, keep_var_names=None, output_names=None, clear_devices=True):
